63eb9aa7f0e98b284f12fd288a3807528552b97d
fruxc <hammadansari17@gmail.com>
2020-04-27 15:00:42 +0530

    Assignment 1

---
+++ components/MainComponent.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import DishDetail from "./DishDetailComponent";
+import Home from "./HomeComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
+import { View, Platform } from "react-native";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
+
+const MenuNavigator = createStackNavigator(
+  {
+    Menu: { screen: Menu },
+    DishDetail: { screen: DishDetail },
+  },
+  {
+    initialRouteName: "Menu",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+    }),
+  }
+);
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+    }),
+  }
+);
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+    }),
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+    Contact: {
+      screen: ContactNavigator,
+      navigationOptions: {
+        title: "Contact",
+        drawerLabel: "Contact us",
+      },
+    },
+    About: {
+      screen: AboutNavigator,
+      navigationOptions: {
+        title: "About",
+        drawerLabel: "About us",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#D1C4E9",
+  }
+);
+
+class Main extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+export default Main;
---
+++ components/MenuComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { View, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Menu",
+  };
+
+  render() {
+    const menuItem = ({ item, index }) => {
+      return (
+        <ListItem
+          key={index}
+          title={item.name}
+          subtitle={item.description}
+          onPress={() => navigate("DishDetail", { dishId: item.id })}
+          hideChevron={true}
+          leftAvatar={{ source: require("../assets/images/uthappizza.png") }}
+        />
+      );
+    };
+
+    const { navigate } = this.props.navigation;
+
+    return (
+      <FlatList
+        data={this.state.dishes}
+        renderItem={menuItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  }
+}
+
+export default Menu;
